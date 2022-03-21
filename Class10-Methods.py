71132f55073867192e4fa447f203e2c4c10932b8
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-03-21 14:39:12 -0300

    OOP Methods

---
+++ Class10-Methods.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def bark(self):
+        print('Woof!')
+
+
+fido = Dog("Fido", "brown")
+print(fido.name)
+print(fido.color)
+fido.bark()
